24975192eae95ceed356d9123a574e8854dc2dbf
Zhi.Xin <zhi.xin@myob.com>
2024-02-28 14:12:21 +0800

    2.28

---
+++ init.sh
@@ -0,0 +1,6 @@
+git remote add gitee https://gitee.com/zhixin9001/2023_go.git
+git remote add github https://github.com/zhixin9001/2023_go.git
+
+git remote add all https://gitee.com/zhixin9001/2023_go.git
+git remote set-url --add --push all https://gitee.com/zhixin9001/2023_go.git
+git remote set-url --add --push all https://github.com/zhixin9001/2023_go.git
