0ac4be6b7730b3697c45d7c070757432808539f4
Zhi.Xin <zhi.xin@myob.com>
2024-02-29 12:59:30 +0800

    2.29

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 2023_Go
+
+go build 1.go
+./1.exe 
+go run 1.go
--- codes/1.go
+++ codes/1.go
@@ -5,11 +5,26 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 
-	//变量声明
-	var a int32
-	var s string = "hello"
-	var i = 12
-	n := 17
-	fmt.Println(a, s, i, n)
+	// // 变量声明
+	// var a int32
+	// var s string = "hello"
+	// var i = 12
+	// n := 17
+	// fmt.Println(a, s, i, n)
 
+	// var a = 17
+	// var b int32 = 17
+	// var c = int32(17)
+	// var (
+	// 	d = 17
+	// 	e = float32(3.14)
+	// )
+	// var f int32
+	// var g float32
+	// fmt.Println(a, b, c, d, e, f, g)
+
+	var a = 1
+	b := 2
+	c := int32(3)
+	fmt.Println(a, b, c)
 }
