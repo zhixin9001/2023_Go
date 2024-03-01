4f2cf2149ee4d7fbc1bef59a2e661b738325c2ad
Zhi.Xin <zhi.xin@myob.com>
2024-03-01 13:22:43 +0800

    3.1

--- codes/1.go
+++ codes/1.go
@@ -23,8 +23,48 @@ func main() {
 	// var g float32
 	// fmt.Println(a, b, c, d, e, f, g)
 
-	var a = 1
-	b := 2
-	c := int32(3)
-	fmt.Println(a, b, c)
+	// var a = 1
+	// b := 2
+	// c := int32(3)
+	// fmt.Println(a, b, c)
+
+	// const (
+	// 	A, B = 1, 2
+	// 	C, D
+	// )
+	// fmt.Println(A, B, C, D)
+
+	// iota: const声明块（包括单行声明）中每个常量所处位置在块中的偏移值（从零开始）
+	// const (
+	// 	A, B = iota, iota + 10
+	// 	C, D
+	// )
+	// fmt.Println(A, B, C, D)
+	// const (
+	// 	_ = iota // 略过0
+	// 	A
+	// 	B
+	// )
+
+	// fmt.Println(A, B)
+
+	// const (
+	// 	PI   = 3.14 //非整形常量
+	// 	PI_2 = 3.1414
+	// )
+
+	// type Weekday int
+	// const (
+	// 	Sunday = Weekday(iota) //有类型枚举常量
+	// 	Monday
+	// 	Tuesday
+	// )
+	// fmt.Println(Sunday, Monday, Tuesday)
+
+	//复合字面值简化赋值
+	var a [2]int
+	a[0] = 1
+	a[1] = 2
+	var b = [2]int{1, 2}
+	fmt.Println(a, b)
 }
